7e340467bbe11e462f2c3b3994009e8e543bd3fc
Kallol001 <121399773+Kallol001@users.noreply.github.com>
2023-12-17 20:55:47 +0530

    first Commit

---
+++ lib/common/show_model.dart
@@ -0,0 +1,223 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gap/gap.dart';
+import 'package:intl/intl.dart';
+import 'package:todo/constants/app_style.dart';
+import 'package:todo/model/todo_model.dart';
+import 'package:todo/provider/date_time_provider.dart';
+import 'package:todo/provider/radio_provider.dart';
+import 'package:todo/provider/service_provider.dart';
+import 'package:todo/widget/date_time_widget.dart';
+import 'package:todo/widget/radio_widget.dart';
+import 'package:todo/widget/textfield_widget.dart';
+
+class AddNewTaskModel extends ConsumerWidget {
+  AddNewTaskModel({
+    super.key,
+  });
+
+  final titleController = TextEditingController();
+  final descriptionController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final dateProv = ref.watch(dateProvider);
+    return Container(
+      padding: const EdgeInsets.all(30),
+      height: MediaQuery.of(context).size.height * 0.90,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            width: double.infinity,
+            child: Text(
+              'New Task To Do',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+            ),
+          ),
+          Divider(
+            thickness: 1.2,
+            color: Colors.grey.shade200,
+          ),
+          const Gap(12),
+          const Text(
+            'Task Title',
+            style: AppStyle.headingOne,
+          ),
+          const Gap(6),
+          TextFieldWidget(
+            maxLine: 1,
+            hintText: 'Add Task Name',
+            txtController: titleController,
+          ),
+          const Gap(12),
+          const Text('Description', style: AppStyle.headingOne),
+          const Gap(6),
+          TextFieldWidget(
+            maxLine: 5,
+            hintText: 'Add Description',
+            txtController: descriptionController,
+          ),
+          const Gap(12),
+          const Text('Category', style: AppStyle.headingOne),
+          Row(
+            children: [
+              Expanded(
+                child: RadioWidget(
+                  categColor: Colors.green.shade700,
+                  titleRadio: 'LRN',
+                  valueInput: 1,
+                  onChangeValue: () =>
+                      ref.read(radioProvider.notifier).update((state) => 1),
+                ),
+              ),
+              Expanded(
+                child: RadioWidget(
+                  categColor: Colors.blue.shade700,
+                  titleRadio: 'WRK',
+                  valueInput: 2,
+                  onChangeValue: () =>
+                      ref.read(radioProvider.notifier).update((state) => 2),
+                ),
+              ),
+              Expanded(
+                child: RadioWidget(
+                  categColor: Colors.amberAccent.shade700,
+                  titleRadio: 'GEN',
+                  valueInput: 3,
+                  onChangeValue: () =>
+                      ref.read(radioProvider.notifier).update((state) => 3),
+                ),
+              ),
+            ],
+          ),
+
+          // Date And Time
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              DateTimeWidget(
+                titleText: 'Date',
+                valueText: dateProv,
+                iconSection: CupertinoIcons.calendar,
+                onTap: () async {
+                  final getValue = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2021),
+                      lastDate: DateTime(2050));
+
+                  if (getValue != null) {
+                    final format = DateFormat.yMd();
+                    ref
+                        .read(dateProvider.notifier)
+                        .update((state) => format.format(getValue));
+                  }
+                },
+              ),
+              const Gap(22),
+              DateTimeWidget(
+                titleText: 'Time',
+                valueText: ref.watch(timeProvider),
+                iconSection: CupertinoIcons.clock,
+                onTap: () async {
+                  final getTime = await showTimePicker(
+                    context: context,
+                    initialTime: TimeOfDay.now(),
+                  );
+                  if (getTime != null) {
+                    ref
+                        .read(timeProvider.notifier)
+                        .update((state) => getTime.format(context));
+                  }
+                },
+              ),
+            ],
+          ),
+
+          //Button Section
+          const Gap(12),
+          Row(
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.blue.shade800,
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    side: BorderSide(
+                      color: Colors.blue.shade800,
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 14),
+                  ),
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Cancel'),
+                ),
+              ),
+              const Gap(20),
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue.shade800,
+                    foregroundColor: Colors.white,
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 14),
+                  ),
+                  onPressed: () {
+                    final getRadioValue = ref.read(radioProvider);
+                    String category = '';
+
+                    switch (getRadioValue) {
+                      case 1:
+                        category = 'Learning';
+                        break;
+                      case 2:
+                        category = 'Working';
+                        break;
+                      case 3:
+                        category = 'General';
+                        break;
+                    }
+
+                    ref.read(serviceProvider).addNewTask(TodoModel(
+                          taskTitle: titleController.text,
+                          description: descriptionController.text,
+                          category: category,
+                          dateTask: ref.read(dateProvider),
+                          timeTask: ref.read(timeProvider),
+                          isDone: false,
+                        ));
+
+                    print('Data is Saved');
+
+                    titleController.clear();
+                    descriptionController.clear();
+                    ref.read(radioProvider.notifier).update((state) => 0);
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Create'),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/view/home_page.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gap/gap.dart';
+import 'package:todo/provider/service_provider.dart';
+
+import '../common/show_model.dart';
+import '../widget/card_todo_widget.dart';
+
+class HomePage extends ConsumerWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final todoData = ref.watch(fetchDataProvider);
+    return Scaffold(
+      backgroundColor: Colors.grey.shade200,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 0,
+        title: ListTile(
+          leading: ClipOval(
+            child: CircleAvatar(
+              backgroundColor: Colors.amber.shade200,
+              radius: 25,
+              child: Image.asset(
+                'assets/avatar.png',
+                fit: BoxFit.cover, // Set the fit property to cover
+              ),
+            ),
+          ),
+          title: const Text(
+            'Hi, Kallol',
+            style: TextStyle(
+                fontWeight: FontWeight.bold, color: Colors.black, fontSize: 24),
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(CupertinoIcons.calendar),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(CupertinoIcons.bell),
+              ),
+            ]),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(children: [
+          const Gap(12),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Today\'s Task',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                  ),
+                  // Text(
+                  //   'Wednesday, 11 May',
+                  //   style: TextStyle(color: Colors.grey),
+                  // ),
+                ],
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFFD5E8FA),
+                    foregroundColor: Colors.blue.shade800,
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8))),
+                onPressed: () => showModalBottomSheet(
+                  isScrollControlled: true,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16)),
+                  context: context,
+                  builder: (context) => AddNewTaskModel(),
+                ),
+                child: const Text(
+                  '+ New Task',
+                ),
+              ),
+            ],
+          ),
+
+          //Card List
+          const Gap(20),
+          ListView.builder(
+            itemCount: todoData.value?.length ?? 0,
+            shrinkWrap: true,
+            itemBuilder: (context, index) =>
+                CardTodoListWidget(getIndex: index),
+          )
+        ]),
+      )),
+    );
+  }
+}
